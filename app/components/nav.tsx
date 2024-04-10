1fccb921f458d5b01281a79a16340ef4b9b130dc
rakshitmeshram <rakshitmeshram10@gmail.com>
2024-04-10 17:19:45 +0530

    sticky nav

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -15,7 +15,7 @@ const navItems = {
 export function Navbar() {
   return (
     <aside className="-ml-[8px] mb-16 tracking-tight">
-      <div className="lg:sticky lg:top-20">
+      <div className="sticky top-20">
         <nav
           className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
           id="nav"
